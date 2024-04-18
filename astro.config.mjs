0aca714ae3a1ff55b069532570241c2a92eff6fd
Dan Stroot <dan.stroot@gmail.com>
2024-04-18 09:06:18 -0700

    initial tailwind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,33 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import { remarkReadingTime } from "./src/utils/readTime.ts";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    drafts: true,
+    shikiConfig: {
+      theme: "material-theme-palenight",
+      wrap: true,
+    },
+  },
+  image: {
+    domains: ["https://danstroot.imgix.net"],
+  },
   integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        experimentalThemes: {
+          light: "vitesse-light",
+          dark: "material-theme-palenight",
+        },
+        wrap: true,
+      },
+      drafts: true,
+    }),
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,11 @@
-export { sluglify, unsluglify } from './sluglify'
-export { cn } from './cn'
+export { sluglify, unsluglify } from "./sluglify";
+export { cn } from "./cn";
 export {
-	getCategories,
-	getPosts,
-	getPosts2,
-	getTags,
-	getPostByTag,
-	filterPostsByCategory
-} from './post'
-export { remarkReadingTime } from './readTime'
+  // getCategories,
+  // getPosts,
+  getPosts2,
+  // getTags,
+  // getPostByTag,
+  // filterPostsByCategory
+} from "./post";
+export { remarkReadingTime } from "./readTime";
