68ebe1298c9de3982ba3488948f377480d6ebc87
Dan Stroot <dan.stroot@gmail.com>
2024-04-18 12:09:20 -0700

    working blog

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,25 @@ export default {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography() {
+        return {
+          lg: {
+            css: [
+              {
+                li: {
+                  marginTop: "0",
+                  marginBottom: "0",
+                },
+                cite: {
+                  fontSize: "1.0rem",
+                },
+              },
+            ],
+          },
+        };
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
