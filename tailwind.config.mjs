5584965175da1a57373a97211b2991f072862ef1
Dan Stroot <dan.stroot@gmail.com>
2024-06-14 09:25:25 -0700

    tweaks

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,7 +15,76 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      typography() {
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        footerbg: 'hsl(var(--footerbg))',
+        foreground: 'hsl(var(--foreground))',
+        footerfg: 'hsl(var(--footerfg))',
+        accent2: {
+          DEFAULT: 'hsl(var(--accent2))',
+          foreground: 'hsl(var(--accent-foreground2))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      letterSpacing: {
+        tighter: '-.05em',
+      },
+      lineHeight: {
+        tight: '1.2',
+      },
+      outline: {
+        transparent: '2px #983799',
+      },
+      typography(theme: any) {
         return {
           DEFAULT: {
             css: [disabledCss],
@@ -31,8 +100,9 @@ export default {
                   fontSize: '1.0rem',
                 },
                 hr: {
-                  marginTop: '0',
-                  marginBottom: '1.5rem',
+                  marginTop: '.75rem',
+                  marginBottom: '1.25rem',
+                  border: '0.75px solid',
                 },
               },
             ],
